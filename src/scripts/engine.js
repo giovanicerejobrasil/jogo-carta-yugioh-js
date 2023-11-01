921d82c8cfb446f112c0329dc1b339f1789fad9e
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2023-11-01 12:19:31 -0300

    Update engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,9 +1,1 @@
-document.addEventListener("contextmenu", (event) => {
-  event.preventDefault();
-});
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "F12") {
-    event.preventDefault();
-  }
-});
