59a29f27046d84cb6444fb040b8886e1f4ab719b
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2023-11-02 11:18:41 -0300

    Update engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,6 +18,78 @@ const state = {
   },
 };
 
-function init() {}
+const playersSides = {
+  player1: "player-cards",
+  player2: "computer-cards",
+};
+
+const pathImages = "./src/assets/icons";
+
+const cardData = [
+  {
+    id: 0,
+    name: "Blue Eyes White Dragon",
+    type: "Paper",
+    img: `${pathImages}/dragon.png`,
+    winOf: [1],
+    loseOf: [2],
+  },
+  {
+    id: 1,
+    name: "Dark Magician",
+    type: "Rock",
+    img: `${pathImages}/magician.png`,
+    winOf: [2],
+    loseOf: [0],
+  },
+  {
+    id: 2,
+    name: "Exodia",
+    type: "Scissors",
+    img: `${pathImages}/exodia.png`,
+    winOf: [0],
+    loseOf: [1],
+  },
+];
+
+async function getRandomCardID() {
+  const randomIndex = Math.floor(Math.random() * cardData.length);
+
+  return cardData[randomIndex].id;
+}
+
+async function createCardImage(idCard, fieldSide) {
+  const cardImage = document.createElement("img");
+  cardImage.setAttribute("height", "100px");
+  cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+  cardImage.setAttribute("data-id", idCard);
+  cardImage.classList.add("card");
+
+  if (fieldSide === playersSides.player1) {
+    cardImage.addEventListener("click", () => {
+      setCardsField(cardImage.getAttribute("data-id"));
+    });
+  }
+
+  cardImage.addEventListener("mouseover", () => {
+    drawSelectedCard(idCard);
+  });
+
+  return cardImage;
+}
+
+async function drawCards(cardNumbers, fieldSide) {
+  for (let i = 0; i < cardNumbers; i++) {
+    const randomIdCard = await getRandomCardID();
+    const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+    document.getElementById(fieldSide).appendChild(cardImage);
+  }
+}
+
+function init() {
+  drawCards(5, playersSides.player1);
+  drawCards(5, playersSides.player2);
+}
 
 init();
